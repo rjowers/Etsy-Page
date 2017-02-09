89b3b716b2b632455319be422e5b2f9f90fc37b7
Ryan Jowers <rjowers@g.clemson.edu>
2017-02-09 15:24:28 -0500

    finshed header, nav, and sidebar

---
+++ main.js
@@ -0,0 +1,13 @@
+
+$('.alltheitems').html('Hello World')
+
+/*
+function boardTemplate (board) {
+  var spacesHtml = board.map(function (x) { return `<div class="space">${x}</div>`; });
+  return `
+    <div class="board">
+      ${spacesHtml.join("\n")}
+    </div>
+  `;
+}
+*/
