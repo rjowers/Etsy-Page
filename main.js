6fb419b9fb2543db29f828876eb82c4fc410337e
Ryan Jowers <rjowers@g.clemson.edu>
2017-02-11 09:11:23 -0500

    finished header, added links to all items

--- main.js
+++ main.js
@@ -1,33 +1,32 @@
-
 //for image -> items.results[0].Images[0].url_75x75
 
 
 //var box = `
 //    <img class="whiskey-pics" src="https://img0.etsystatic.com/126/0/11063629/il_75x75.874258770_it05.jpg">
 
-  //  </div>
+//  </div>
 //`
 
 
 //function pictures () {
-  //for (var i = 0; i < result.length; i++) {
-    //items[i]
-  //}
-  //console.log(pictures)
+//for (var i = 0; i < result.length; i++) {
+//items[i]
+//}
+//console.log(pictures)
 //}
 
 
 var putImages;
-for(var count = 0; count < items.results.length; count++){
-  putImages = $(".alltheitems").html();
-  putImages +=
-  `<div class="image"><a href = "${items.results[count].url}"><img src="${items.results[count].Images[0].url_170x135}" width = "225"/></a>
+for (var count = 0; count < items.results.length; count++) {
+    putImages = $(".alltheitems").html();
+    putImages +=
+        `<div class="image"><a href = "${items.results[count].url}"><img src="${items.results[count].Images[0].url_170x135}" width = "225"/></a>
    <p class="title">${items.results[count].title}</p>
   <p class="shopname">${items.results[count].Shop.shop_name}</p>
   <p class="money">${items.results[count].price}</p>
   <a href = ${items.results[count].url}</a>
    </div>`;
-  $(".alltheitems").html(putImages);
+    $(".alltheitems").html(putImages);
 };
 
 
